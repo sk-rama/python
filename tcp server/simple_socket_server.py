76de919fece1cbe4ffc9b6902eb1b2a4556e178a
sk-rama <35261782+sk-rama@users.noreply.github.com>
2021-08-31 13:56:22 +0200

    Update simple_socket_server.py

--- tcp server/simple_socket_server.py
+++ tcp server/simple_socket_server.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# source: https://pymotw.com/2/SocketServer/
+
 import os
 import logging
 import sys
@@ -36,69 +38,53 @@ class ForkingRequestHandler(socketserver.BaseRequestHandler):
         self.logger.debug('finish')
         return socketserver.BaseRequestHandler.finish(self)
 
-class ForkingTcpServer(socketserver.ForkingTCPServer):
-    
+class ForkingTcpServer(socketserver.ForkingTCPServer, socketserver.TCPServer):
     def __init__(self, server_address, handler_class=ForkingRequestHandler):
         self.logger = logging.getLogger('ForkingTcpServer')
         self.logger.debug('__init__')
-        socketserver.TCPServer.__init__(self, server_address, handler_class)
+        socketserver.ForkingTCPServer.__init__(self, server_address, handler_class)
         return
 
     def server_activate(self):
         self.logger.debug('server_activate')
-        socketserver.TCPServer.server_activate(self)
-        return
-
-    def serve_forever(self):
-        self.logger.debug('waiting for request')
-        self.logger.info('Handling requests, press <Ctrl-C> to quit')
-        while True:
-            self.handle_request()
+        socketserver.ForkingTCPServer.server_activate(self)
         return
 
     def handle_request(self):
         self.logger.debug('handle_request')
-        return socketserver.TCPServer.handle_request(self)
+        return socketserver.ForkingTCPServer.handle_request(self)
 
     def verify_request(self, request, client_address):
         self.logger.debug('verify_request(%s, %s)', request, client_address)
-        return socketserver.TCPServer.verify_request(self, request, client_address)
+        return socketserver.ForkingTCPServer.verify_request(self, request, client_address)
 
     def process_request(self, request, client_address):
         self.logger.debug('process_request(%s, %s)', request, client_address)
-        return socketserver.TCPServer.process_request(self, request, client_address)
+        return socketserver.ForkingTCPServer.process_request(self, request, client_address)
 
     def server_close(self):
         self.logger.debug('server_close')
-        return socketserver.TCPServer.server_close(self)
+        return socketserver.ForkingTCPServer.server_close(self)
 
     def finish_request(self, request, client_address):
         self.logger.debug('finish_request(%s, %s)', request, client_address)
-        return socketserver.TCPServer.finish_request(self, request, client_address)
+        return socketserver.ForkingTCPServer.finish_request(self, request, client_address)
 
     def close_request(self, request_address):
         self.logger.debug('close_request(%s)', request_address)
-        return socketserver.TCPServer.close_request(self, request_address)
+        return socketserver.ForkingTCPServer.close_request(self, request_address)
+
 
 if __name__ == '__main__':
     import socket
     import threading
 
-    HOST, PORT = "localhost", 9999
+    HOST, PORT = "0.0.0.0", 9999
 
-#    address = (HOST, PORT) # let the kernel give us a port
-#    server = ForkingTcpServer(address, ForkingRequestHandler)
-#    ip, port = server.server_address # find out what port we were given
-#
-#    t = threading.Thread(target=server.serve_forever)
-#    t.setDaemon(False) # don't hang on exit
-#    t.start()
-#
-#    print('Server loop running in process:', os.getpid())
-#
-#    logger = logging.getLogger('client')
-#    logger.info('Server on %s:%s', ip, port)
 with ForkingTcpServer((HOST, PORT), ForkingRequestHandler) as server:
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
+    logger = logging.getLogger('server')
+    print('Server loop running in process:', os.getpid())
+    logger.info('Server on %s:%s', HOST, PORT)
     server.serve_forever()

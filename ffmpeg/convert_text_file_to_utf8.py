790acccdb2e007bc6dbbd4c6443949864ae3f980
sk-rama <35261782+sk-rama@users.noreply.github.com>
2023-01-30 17:15:25 +0100

    Create convert_text_file_to_utf8.py

---
+++ ffmpeg/convert_text_file_to_utf8.py
@@ -0,0 +1,18 @@
+#!python
+import pathlib
+
+#dirs   = ["g:\\JDownloader\\HD Perfect Strangers (2016)\\"]
+dirs   = ["g:\\dokumenty\\BBC Horizon\\BBC Horizon How Many People Can Live On Planet Earth (2009)\\"]
+ext    = ['.srt', '.txt']
+append = "utf8"
+
+files = [file for dir in dirs for file in pathlib.Path(dir).glob('**/*') if file.exists() and file.is_file() and file.suffix in ext]
+
+
+for file in files:
+    input_file  = file;
+    new_name    = file.stem + append + file.suffix
+    output_file = file.with_name(new_name)
+    with open(input_file, 'r', encoding='cp1250', errors ='replace') as inp, open(output_file, 'w', encoding='utf-8') as outp:
+        for line in inp:
+            outp.write(line)
